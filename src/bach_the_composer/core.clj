329b31688b4c0543a844e6f65648bc2b60ca9f86
Michael Xu <michael.xu1816@gmail.com>
2024-05-14 09:05:48 -0400

    Rename import

--- src/bach_the_composer/core.clj
+++ src/bach_the_composer/core.clj
@@ -1,8 +1,8 @@
 (ns bach-the-composer.core
   (:gen-class)
   (:require [bach-the-composer.table :as table])
-  (:require [bach-the-composer.core-ops :as coreops])
-  (:require [bach-the-composer.derived-ops :as derivedops]))
+  (:require [bach-the-composer.core-ops :as core-ops])
+  (:require [bach-the-composer.derived-ops :as derived-ops]))
 
 (defn -main
   "Core and derived relational algebra operations"
@@ -30,24 +30,24 @@
                   [456 "abc"]
                   [456 "gov"]})]
 
-    (table/print-table (coreops/selection user #(> (:uid %) 125)))
-    (table/print-table (coreops/projection user [:age]))
-    (table/print-table (coreops/rename user "BROO" {:uid :broskid :age :years}))
+    (table/print-table (core-ops/selection user #(> (:uid %) 125)))
+    (table/print-table (core-ops/projection user [:age]))
+    (table/print-table (core-ops/rename user "BROO" {:uid :broskid :age :years}))
     (table/print-table member)
-    (let [a (coreops/rename-table (coreops/selection member #(> (:uid %) 200)) "A")
-          b (coreops/rename-table (coreops/selection member #(< (:uid %) 500)) "B")]
+    (let [a (core-ops/rename-table (core-ops/selection member #(> (:uid %) 200)) "A")
+          b (core-ops/rename-table (core-ops/selection member #(< (:uid %) 500)) "B")]
       (table/print-table a)
       (table/print-table b)
-      (table/print-table (coreops/rename-table (coreops/union a b) "A U B"))
-      (table/print-table (coreops/rename-table (coreops/difference a b) "A - B"))
-      (table/print-table (coreops/rename-table (coreops/difference b a) "B - A"))
-      (table/print-table (coreops/rename-table (derivedops/intersection a b) "A ∩ B")))
+      (table/print-table (core-ops/rename-table (core-ops/union a b) "A U B"))
+      (table/print-table (core-ops/rename-table (core-ops/difference a b) "A - B"))
+      (table/print-table (core-ops/rename-table (core-ops/difference b a) "B - A"))
+      (table/print-table (core-ops/rename-table (derived-ops/intersection a b) "A ∩ B")))
 
     (table/print-table user)
     (table/print-table group)
-    (table/print-table (coreops/rename-table (coreops/cross-product user member) "User X Member"))
-    (table/print-table (coreops/rename-table (derivedops/theta-join user member #(= (:User.uid %) (:Member.uid %))) "User ⋈_{User.uid=Member.uid} member"))
-    (table/print-table (coreops/rename-table (derivedops/natural-join user member) "User ⋈ member"))
+    (table/print-table (core-ops/rename-table (core-ops/cross-product user member) "User X Member"))
+    (table/print-table (core-ops/rename-table (derived-ops/theta-join user member #(= (:User.uid %) (:Member.uid %))) "User ⋈_{User.uid=Member.uid} member"))
+    (table/print-table (core-ops/rename-table (derived-ops/natural-join user member) "User ⋈ member"))
 
     ;; (table/print (selection (rename-table (cross-product user member) "User X Member") #(> (:age %) )))
     ;; (println (:key-to-idx user))
